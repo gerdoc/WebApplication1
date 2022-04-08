f3766fb179074456c6ab225c88e2def027f8d85c
gerdoc <ggerdoc@gmail.com>
2022-04-08 12:28:57 -0500

    Se agrega método para leer tabla

---
+++ src/java/org/gerdoc/proyecto1/dao/Alumno2.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.gerdoc.proyecto1.dao;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Alumno2 
+{
+    private Integer id;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+
+    public Alumno2() 
+    {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+    
+    
+}
--- src/java/org/gerdoc/proyecto1/service/MySqlConnection.java
+++ src/java/org/gerdoc/proyecto1/service/MySqlConnection.java
@@ -8,9 +8,13 @@ package org.gerdoc.proyecto1.service;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import org.gerdoc.proyecto1.dao.Alumno;
+import org.gerdoc.proyecto1.dao.Alumno2;
 
 /**
  *
@@ -55,6 +59,72 @@ public class MySqlConnection implements Serializable
         return null;
     }
     
+    public static Connection getConnection( )
+    {
+        return getConnection("TEST", "1234", "Gerdoc", "127.0.0.1" );
+    }
+    
+    public static List<Alumno2> getAlumnosList( )
+    {
+        List<Alumno2>alumno2s = null;
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        Alumno2 alumno2 = null;
+        
+        try 
+        {
+            connection = getConnection( );
+            if( connection == null )
+            {
+                return null;
+            }
+            statement = connection.createStatement( );
+            if( statement == null )
+            {
+                return null;
+            }
+            resultSet = statement.executeQuery( "SELECT * FROM TBL_ALUMNO" );
+            if( resultSet == null )
+            {
+                return null;
+            }
+            alumno2s = new ArrayList<>();
+            while( resultSet.next() )
+            {
+                alumno2 = new Alumno2();
+                alumno2.setId( resultSet.getInt(1) );
+                alumno2.setNombre( resultSet.getString(2) );
+                alumno2.setApellidoP(resultSet.getString(3) );
+                alumno2.setApellidoM(resultSet.getString(4) );
+                alumno2s.add(alumno2);
+            }
+            resultSet.close();
+            closeConnection(connection);
+            return alumno2s;
+        } 
+        catch (SQLException ex) 
+        {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static void printAlumno2s( List<Alumno2> alumno2s )
+    {
+        if( alumno2s == null || alumno2s.size() == 0)
+        {
+            return;
+        }
+        for( Alumno2 alumno2 : alumno2s )
+        {
+            System.out.println("ID=" +alumno2.getId());
+            System.out.println("NOMBRE="+alumno2.getNombre());
+            System.out.println("APELLIDO P="+alumno2.getApellidoP());
+            System.out.println("APELLIDO M="+alumno2.getApellidoM());
+            
+        }
+    }
     public static void closeConnection( Connection connection )
     {
         try 
@@ -79,19 +149,22 @@ public class MySqlConnection implements Serializable
     public static void main( String a[] )
     {
         Connection connection = null;
+        List<Alumno2>alumno2s = null;
         if( !testDriver() )
         {
             System.out.println("Mysql Test Driver Not ok");
             return;
         }
         System.out.println("Mysql Test Driver Ok");
-        connection = getConnection("test", "1234", "EmMax", "192.168.50.187" );
+        connection = getConnection("TEST", "1234", "Gerdoc", "127.0.0.1" );
         if( connection == null )
         {
             System.out.println("MySqlConnection.getConnection not ok");
             return;
         }
         System.out.println("MySqlConnection.getConnection ok");
+        alumno2s = getAlumnosList();
+        printAlumno2s(alumno2s);
         closeConnection(connection);
         System.out.println("MySqlConnection.closeConnection ok");
     }
